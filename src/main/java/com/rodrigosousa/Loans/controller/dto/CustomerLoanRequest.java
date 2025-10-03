8bf0da01796d8fbdee6543778ffdec6984de0ab3
Rodrigo <rodrigudesousa@gmail.com>
2025-10-03 16:58:36 -0400

    feat: service checkLoanAvailability method

--- src/main/java/com/rodrigosousa/Loans/controller/dto/CustomerLoanRequest.java
+++ src/main/java/com/rodrigosousa/Loans/controller/dto/CustomerLoanRequest.java
@@ -1,8 +1,14 @@
 package com.rodrigosousa.Loans.controller.dto;
 
+import com.rodrigosousa.Loans.domain.Customer;
+
 public record CustomerLoanRequest(Integer age,
                                   String cpf,
                                   String name,
                                   Double income,
                                   String location) {
+
+    public Customer toCustomer() {
+        return new Customer(age, cpf, name, income, location);
+    }
 }
--- src/main/java/com/rodrigosousa/Loans/service/LoanService.java
+++ src/main/java/com/rodrigosousa/Loans/service/LoanService.java
@@ -2,14 +2,35 @@ package com.rodrigosousa.Loans.service;
 
 import com.rodrigosousa.Loans.controller.dto.CustomerLoanReponse;
 import com.rodrigosousa.Loans.controller.dto.CustomerLoanRequest;
+import com.rodrigosousa.Loans.controller.dto.LoanResponse;
+import com.rodrigosousa.Loans.domain.Loan;
+import com.rodrigosousa.Loans.domain.LoanType;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoanService {
 
-    public CustomerLoanReponse checkLoanAvailability(CustomerLoanRequest request) {
-        
+    public CustomerLoanReponse checkLoanAvailability(CustomerLoanRequest loanRequest) {
 
-        return null;
+        var customer = loanRequest.toCustomer();
+        var loan = new Loan(customer);
+
+        List<LoanResponse> loans = new ArrayList<>();
+        if(loan.isPersonalLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.PERSONAL, loan.getPersonalLoanInterestRate()));
+        }
+
+        if(loan.isConsigmentedLoanAvailable()){
+            loans.add(new LoanResponse(LoanType.CONSIGMENTED, loan.getConsigmentedLoanInterestRate()));
+        }
+
+        if(loan.isGaranteedLoanAvailable()){
+            loans.add(new LoanResponse(LoanType.GARANTEED, loan.getGaranteedLoanInterestRate()));
+        }
+
+        return new CustomerLoanReponse(loanRequest.name(), loans);
     }
 }
