218c22f33831b5222c0422100e7f56495ea87216
Rodrigo <rodrigudesousa@gmail.com>
2025-10-03 16:24:56 -0400

    feat: create service Loan

--- src/main/java/com/rodrigosousa/Loans/controller/LoanController.java
+++ src/main/java/com/rodrigosousa/Loans/controller/LoanController.java
@@ -2,6 +2,7 @@ package com.rodrigosousa.Loans.controller;
 
 import com.rodrigosousa.Loans.controller.dto.CustomerLoanReponse;
 import com.rodrigosousa.Loans.controller.dto.CustomerLoanRequest;
+import com.rodrigosousa.Loans.service.LoanService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoanController {
 
+    private final LoanService loanService;
+
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
     @PostMapping(value = "/customers-loans")
     public ResponseEntity<CustomerLoanReponse> customersLoans(@RequestBody CustomerLoanRequest request) {
-
-        return null;
+        var loanResponse = loanService.checkLoanAvailability(request);
+        return ResponseEntity.ok(loanResponse);
     }
 }
---
+++ src/main/java/com/rodrigosousa/Loans/service/LoanService.java
@@ -0,0 +1,15 @@
+package com.rodrigosousa.Loans.service;
+
+import com.rodrigosousa.Loans.controller.dto.CustomerLoanReponse;
+import com.rodrigosousa.Loans.controller.dto.CustomerLoanRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanService {
+
+    public CustomerLoanReponse checkLoanAvailability(CustomerLoanRequest request) {
+        
+
+        return null;
+    }
+}
